f45b20f0e54068e0b11a5d0a273bdd0346161c68
Oğuzhan Akkoyunlu <92020160+oguzhanakk@users.noreply.github.com>
2022-12-05 17:40:10 +0300

    Add files via upload

---
+++ anakodun_dolar_euro_kismi.py
@@ -0,0 +1,17 @@
+import datetime
+from pandas_datareader import data as pdr
+
+# Bugünün tarihini alın ve bir string olarak biçimlendirin
+today = datetime.datetime.today().strftime("%d/%m/%Y")
+
+# USD/TRY döviz kuru için geçmiş verileri çekin ve 'usdtrydf' adlı bir veri kümesine kaydedin
+usdtrydf = pdr.get_data_yahoo("USDTRY=X", start="2019-01-01", end={today})
+usdtrydf = usdtrydf[["Close"]].rename(columns={"Close": "USD_Close"})
+
+# EUR/TRY döviz kuru için geçmiş verileri çekin ve 'eurtrydf' adlı bir veri kümesine kaydedin
+eurtrydf = pdr.get_data_yahoo("EURTRY=X", start="2019-01-01", end={today})
+eurtrydf = eurtrydf[["Close"]].rename(columns={"Close": "EUR_Close"})
+
+# Veri kümelerini CSV dosyalarına kaydetmeyi burada ekleyebilirsiniz
+usdtrydf.to_csv("usdtry.csv")
+eurtrydf.to_csv("eurtrydf.csv")
